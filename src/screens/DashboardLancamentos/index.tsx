a2370a88a0fad61cf86aa2b6268bb0ec313ed0ab
castelvani <ggnei@hotmail.com>
2021-03-02 16:59:13 -0300

    Added historic page to navigation bar

--- src/screens/DashboardLancamentos/index.tsx
+++ src/screens/DashboardLancamentos/index.tsx
@@ -1,10 +1,49 @@
 import React from "react";
 import { View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
 
-// import { Container } from './styles';
+import { Card, CardTitle, Balance, Historic, CardSubTitle } from "./style";
 
 const DashboardLancamentos: React.FC = () => {
-  return <View />;
+  return (
+    <Card>
+      <CardTitle>
+        <Feather name="dollar-sign" color="#B9B9B9" size={20} /> Últimos
+        lançamentos
+      </CardTitle>
+      <ScrollView>
+        <Historic>
+          <Balance>R$:1.890,00</Balance>
+          <CardSubTitle>11 de Fev</CardSubTitle>
+        </Historic>
+        <Historic>
+          <Balance>R$:1.890,00</Balance>
+          <CardSubTitle>11 de Fev</CardSubTitle>
+        </Historic>
+        <Historic>
+          <Balance>R$:1.890,00</Balance>
+          <CardSubTitle>11 de Fev</CardSubTitle>
+        </Historic>
+        <Historic>
+          <Balance>R$:1.890,00</Balance>
+          <CardSubTitle>11 de Fev</CardSubTitle>
+        </Historic>
+        <Historic>
+          <Balance>R$:1.890,00</Balance>
+          <CardSubTitle>11 de Fev</CardSubTitle>
+        </Historic>
+        <Historic>
+          <Balance>R$:1.890,00</Balance>
+          <CardSubTitle>11 de Fev</CardSubTitle>
+        </Historic>
+        <Historic>
+          <Balance>R$:1.890,00</Balance>
+          <CardSubTitle>11 de Fev</CardSubTitle>
+        </Historic>
+      </ScrollView>
+    </Card>
+  );
 };
 
 export default DashboardLancamentos;
