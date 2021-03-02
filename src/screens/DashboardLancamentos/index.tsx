8e56a541aaee62b3ec0b918b85cb9abf01c33554
castelvani <ggnei@hotmail.com>
2021-03-02 20:51:15 -0300

    Added date picker to dashboard

--- src/screens/DashboardLancamentos/index.tsx
+++ src/screens/DashboardLancamentos/index.tsx
@@ -1,17 +1,45 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
-
+import DateTimePicker from "@react-native-community/datetimepicker";
 import { Card, CardTitle, Balance, Historic, CardSubTitle } from "./style";
+import { Button, View } from "react-native";
 
 const DashboardLancamentos: React.FC = () => {
+  const [dateInicio, setDateInicio] = useState<Date>(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const handleDateInicio = (selectedDate: any) => {
+    const currentDate = selectedDate || dateInicio;
+    setDateInicio(currentDate);
+    console.log(dateInicio);
+  };
+
+  const showDatepicker = () => {
+    setShow(true);
+  };
+
   return (
     <Card>
       <CardTitle>
         <Feather name="dollar-sign" color="#B9B9B9" size={20} /> Últimos
         lançamentos
       </CardTitle>
+      {/* <View>
+        <View>
+          <Button onPress={showDatepicker} title="Show date picker!" />
+        </View>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={new Date(1598051730000)}
+            is24Hour={true}
+            display="default"
+            onChange={handleDateInicio}
+          />
+        )}
+      </View> */}
       <ScrollView>
         <Historic>
           <Balance>R$:1.890,00</Balance>
