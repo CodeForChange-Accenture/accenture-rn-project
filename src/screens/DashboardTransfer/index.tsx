bef40a17f1a4fdf4e59934e8ed9dbf8b44751137
castelvani <ggnei@hotmail.com>
2021-03-02 22:38:15 -0300

    Added redux to transfers

--- src/screens/DashboardTransfer/index.tsx
+++ src/screens/DashboardTransfer/index.tsx
@@ -20,7 +20,13 @@ import api from "../../service";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import jwt_decode from "jwt-decode";
-import { IUser } from "../../interfaces";
+import {
+  ReloadAccountCredit,
+  ReloadAccountRemove,
+} from "../../store/modules/action";
+
+import { IBank, IUser } from "../../interfaces";
+import { useDispatch, useSelector } from "react-redux";
 
 const DashboardTransfer: React.FC = () => {
   const navigation = useNavigation();
@@ -29,6 +35,10 @@ const DashboardTransfer: React.FC = () => {
   const [destino, setDestino] = useState("");
   const [descricao, setDescricao] = useState("");
   const [valor, setValor] = useState("");
+
+  const dispatch = useDispatch();
+  const state = useSelector((state: IBank) => state);
+
   const TokenDecodedValue = async () => {
     const TokenStorage = await AsyncStorage.getItem("@tokenApp");
     if (TokenStorage) {
@@ -40,22 +50,24 @@ const DashboardTransfer: React.FC = () => {
       alert("Erro autenticação");
     }
   };
-  const handleTransaction = async () => {
+  const handleTransactionUser = async () => {
     const token = await AsyncStorage.getItem("@tokenApp");
     const login = await TokenDecodedValue();
     const today = new Date().toISOString().slice(0, 10);
-    if (selectedValue === "credito") {
-      setDestino("");
-    }
+    const valorParaNumero: number = +valor;
+
+    const tipoMovimento = state.plan.filter(
+      (state) => state.tipoMovimento === "TU"
+    );
 
     const postData = {
-      conta: 575,
+      conta: state.banco.contaBanco.id,
       contaDestino: destino,
       data: today,
       descricao: descricao,
       login: login,
-      planoConta: 1125,
-      valor: valor,
+      planoConta: tipoMovimento[0].id,
+      valor: valorParaNumero,
     };
 
     api
@@ -68,6 +80,53 @@ const DashboardTransfer: React.FC = () => {
       .then((response) => {
         if (response.status === 200) {
           alert("Transação realizada com sucesso!");
+          dispatch(ReloadAccountRemove(valorParaNumero));
+          console.log(tipoMovimento[0].tipoMovimento);
+
+          setDestino("");
+        } else {
+          alert("Erro na transação");
+        }
+      })
+      .catch(() => {
+        alert("Erro na transação");
+      });
+
+    setDescricao("");
+    setValor("");
+  };
+
+  const handleTransactionAccount = async () => {
+    const token = await AsyncStorage.getItem("@tokenApp");
+    const login = await TokenDecodedValue();
+    const today = new Date().toISOString().slice(0, 10);
+    const valorParaNumero: number = +valor;
+
+    const tipoMovimento = state.plan.filter(
+      (state) => state.tipoMovimento === "TC"
+    );
+
+    const postData = {
+      conta: state.banco.contaBanco.id,
+      contaDestino: destino,
+      data: today,
+      descricao: descricao,
+      login: login,
+      planoConta: tipoMovimento[0].id,
+      valor: valorParaNumero,
+    };
+
+    api
+      .post(`lancamentos`, postData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("Transação realizada com sucesso!");
+          dispatch(ReloadAccountCredit(valorParaNumero));
         } else {
           alert("Erro na transação");
         }
@@ -126,7 +185,13 @@ const DashboardTransfer: React.FC = () => {
               />
             </CardBody>
             <CardFooter>
-              <TouchableOpacity onPress={handleTransaction}>
+              <TouchableOpacity
+                onPress={
+                  selectedValue === "usuario"
+                    ? handleTransactionUser
+                    : handleTransactionAccount
+                }
+              >
                 <Send>
                   <SendText>Realizar tranferência</SendText>
                   <Feather name="arrow-right" color="white" size={20} />
